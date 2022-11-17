9df7186361dc38a8edb74f6451cc627fcbdb824d
IFQI91 <ifqi90791@gmail.com>
2022-11-17 10:17:46 -0600

    rmod

--- install.R
+++ install.R
@@ -1,4 +1,7 @@
 install.packages(c('ggplot2','GGally','randomForest','caret','naivebayes',
             'dplyr','caret','ROCR','psych','gt'),dependencies = T)
 
-devtools::install_github("KlausVigo/kknn")
+install.packages("kknn")
+install.packages("tidyverse")
+install.packages("rmarkdown")
+writeLines("R_LIBS_USER=/srv/rlibs", "~/.Renviron")
