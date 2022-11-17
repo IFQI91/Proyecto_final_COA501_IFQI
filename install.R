75909810b854ca79ea66a98a2676e0abdb5fc4bb
IFQI91 <ifqi90791@gmail.com>
2022-11-17 11:31:50 -0600

    rc3

--- install.R
+++ install.R
@@ -1,12 +1,12 @@
 install.packages(c('ggplot2','GGally','randomForest','caret','naivebayes',
-            'dplyr','caret','ROCR','psych','gt'),dependencies = T)
+            'dplyr','caret','ROCR','psych','gt','kknn'),dependencies = T)
 
 install.packages("tidyverse")
 install.packages("rmarkdown")
 
 
-install.packages("githubinstall")
-library(githubinstall)
-githubinstall("KlausVigo/kknn")
+#install.packages("githubinstall")
+#library(githubinstall)
+#githubinstall("KlausVigo/kknn")
 
-writeLines("R_LIBS_USER=/srv/rlibs", "~/.Renviron")
+#writeLines("R_LIBS_USER=/srv/rlibs", "~/.Renviron")
