2455db00dbc5aa4595278424786ce823e9e61e9a
IFQI91 <ifqi90791@gmail.com>
2022-11-17 12:58:44 -0600

    Rc7

--- install.R
+++ install.R
@@ -1,12 +1,14 @@
 install.packages(c('GGally','randomForest','caret','naivebayes',
             'caret','ROCR','psych'))
-            
+
+install.packages("kknn")            
 install.packages("tidyverse")
 install.packages("rmarkdown")
-install.packages("/home/jovyan/kknn_1.3.1.tar.gz", repos = NULL, type = "source")
 
 
 
+
+#install.packages("/home/jovyan/kknn_1.3.1.tar.gz", repos = NULL, type = "source")
 #install.packages("githubinstall")
 #library(githubinstall)
 #githubinstall("KlausVigo/kknn")
